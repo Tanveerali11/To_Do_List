0c03141c440fe59354cdae25f4cb8473c814060e
Tanveerali11 <47947199+Tanveerali11@users.noreply.github.com>
2020-11-15 03:34:16 -0800

    complete todo list app with mongoD

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
+const mongoose = require ('mongoose');
 app = express();
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/listdb', {useNewUrlParser:true});
+
+const itemschema ={
+    name: String
+};
+
+const Item = mongoose.model("Item", itemschema);
+
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
+
 app.get('/',(req,res)=>
 {
+    Item.find({},(err,found)=>
+    {
+        res.render("list",{date:date ,items1:found });
 
-    res.render("list",{date:date});
+    });
+
+    
 }
 );
+app.post("/give",(req,res)=>
+{
+    var item= req.body.task;
+    const data = new Item({
+        name:item
+    });
+    
+    data.save();
+    
+    res.redirect("/");
+});
+app.post("/delet",(req,res)=>
+{
+const id =req.body.check;
+
+Item.findByIdAndRemove(id, (err)=>
+{
+    if(!err){
+        console.log("delete successfully");
+    }
+});
+res.redirect("/");
 
 
+});
 
 app.listen(3000,(req,res)=>
 {
